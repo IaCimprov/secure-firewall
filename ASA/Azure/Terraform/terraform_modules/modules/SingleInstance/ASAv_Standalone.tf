66f29d12097a46cb8e894334ea194505f8064d91
dliskevch <d.liskevych@gmail.com>
2023-04-05 10:16:01 +0000

    Added modules

--- ASA/Azure/Terraform/terraform_modules/modules/SingleInstance/ASAv_Standalone.tf
+++ ASA/Azure/Terraform/terraform_modules/modules/SingleInstance/ASAv_Standalone.tf
@@ -314,14 +314,3 @@ resource "azurerm_virtual_machine" "asav-instance" {
   }
   tags = var.tags
 }
-
-################################################################################################################################
-# Output
-################################################################################################################################
-data "azurerm_public_ip" "asav-mgmt-interface" {
-  name                = azurerm_public_ip.asav-mgmt-interface.name
-  resource_group_name = azurerm_virtual_machine.asav-instance.resource_group_name
-}
-output "public_ip_address" {
-  value = data.azurerm_public_ip.asav-mgmt-interface.ip_address
-}
